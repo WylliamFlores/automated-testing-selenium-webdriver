483af3ed9c585bfaee0ffb6283a2d56f2eda7e7e
Wylliam Flores <wylliammf@gmail.com>
2019-11-08 12:19:56 -0300

    screenshot at and of test

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,10 +1,17 @@
 package core;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.TestName;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import pages.LoginPage;
 
@@ -13,7 +20,7 @@ public class BaseTest {
 	private LoginPage page = new LoginPage();
 
 	@Before
-	public void initialize() {
+	public void initialize() {		
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Informatica\\eclipse-workspace\\driver\\geckodriver.exe");
 		DriverFactory.getDriver().get("https://seubarriga.wcaquino.me/login");
 		DriverFactory.getDriver().manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
@@ -21,8 +28,16 @@ public class BaseTest {
 		DriverFactory.getDriver().findElement(By.linkText("reset")).click();
 	}
 
+	@Rule
+	public TestName testName = new TestName();
+
 	@After
-	public void end() {
+	public void end() throws IOException{
+		TakesScreenshot ss = (TakesScreenshot) DriverFactory.getDriver();
+		File archive = ss.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(archive, new File("target" + File.separator + "screenshot" +
+				File.separator + testName.getMethodName() + ".jpg"));
+
 		DriverFactory.killDriver();
 	}
 }
--- src/main/java/tests/LoginTests.java
+++ src/main/java/tests/LoginTests.java
@@ -1,11 +1,18 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import core.DriverFactory;
 import pages.LoginPage;
@@ -22,8 +29,16 @@ public class LoginTests {
 		page = new LoginPage();
 	}
 
+	@Rule
+	public TestName testName = new TestName();
+
 	@After
-	public void end() {
+	public void end()throws IOException{
+		TakesScreenshot ss = (TakesScreenshot) DriverFactory.getDriver();
+		File archive = ss.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(archive, new File("target" + File.separator + "screenshot" +
+				File.separator + testName.getMethodName() + ".jpg"));
+
 		DriverFactory.killDriver();
 	}
 
